b27fb2016929456c317e06f8d3621a7541912430
Nahbuma Gana <nfg23@cornell.edu>
2020-04-25 19:24:36 -0400

    Updates

---
+++ precache-manifest.d45da17bd431a7376c9ca6d3e04b65c9.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd62b9c61f3d8d886d26908119922074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbd74e421f07d221b8b9",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fc868424097259028c23",
+    "url": "/static/css/main.5ea6c5d3.chunk.css"
+  },
+  {
+    "revision": "cbd74e421f07d221b8b9",
+    "url": "/static/js/2.70cdabbf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.70cdabbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc868424097259028c23",
+    "url": "/static/js/main.143e850d.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  },
+  {
+    "revision": "f71f6cf7ef9cd85e95e9f07e2f5173d3",
+    "url": "/static/media/arrow.f71f6cf7.svg"
+  },
+  {
+    "revision": "319ef286e9e6441b3590f154f0b59b11",
+    "url": "/static/media/brandon-text-medium.319ef286.woff"
+  },
+  {
+    "revision": "fe9b57ea0bb24e2d4b48e319ebafc558",
+    "url": "/static/media/brandon-text-regular.fe9b57ea.woff"
+  },
+  {
+    "revision": "de233255b9b1082d8f0efb2da7761db1",
+    "url": "/static/media/commissions.de233255.svg"
+  },
+  {
+    "revision": "9c83e26ef1dbdc464a27170067837f55",
+    "url": "/static/media/customers.9c83e26e.svg"
+  },
+  {
+    "revision": "48ff19429f43061e583295b6ce69cde5",
+    "url": "/static/media/digital.48ff1942.svg"
+  },
+  {
+    "revision": "ce6c7e29d2644a2d7d099cb05a51ae00",
+    "url": "/static/media/efficiency.ce6c7e29.svg"
+  },
+  {
+    "revision": "e8d382e5e0d92d2c53a9a58db434eb47",
+    "url": "/static/media/experience.e8d382e5.svg"
+  },
+  {
+    "revision": "349d75e480659e5fb46ec1d715031e99",
+    "url": "/static/media/gray-logo.349d75e4.svg"
+  },
+  {
+    "revision": "7551ee9a1f6118227928e3cff8e15212",
+    "url": "/static/media/ipad.7551ee9a.svg"
+  },
+  {
+    "revision": "d86dc7b099b98ac387797955cceb4149",
+    "url": "/static/media/iphone.d86dc7b0.svg"
+  },
+  {
+    "revision": "7ab23d2d44c83f16438ac8a329f8c2d9",
+    "url": "/static/media/map.7ab23d2d.svg"
+  },
+  {
+    "revision": "5ff39d2b8216aebb025f38d12fc154a5",
+    "url": "/static/media/phone.5ff39d2b.svg"
+  },
+  {
+    "revision": "7b14855f276e3bb208f9abc51b4df709",
+    "url": "/static/media/restaurant-1.7b14855f.svg"
+  },
+  {
+    "revision": "4976530820e7401f2dd507ff9f9b4d68",
+    "url": "/static/media/restaurant-2.49765308.svg"
+  },
+  {
+    "revision": "b858fb0077f9e0b2a6c542712e215ca8",
+    "url": "/static/media/restaurant-3.b858fb00.svg"
+  },
+  {
+    "revision": "df083ae2c3a30542ff321e4ce5235a61",
+    "url": "/static/media/slant.df083ae2.svg"
+  },
+  {
+    "revision": "8fb879b0c758a71692a369d074abff3f",
+    "url": "/static/media/store.8fb879b0.svg"
+  },
+  {
+    "revision": "4b4c6f3e8c4ed9e2844788c2baa3f2bf",
+    "url": "/static/media/waylogo.4b4c6f3e.svg"
+  }
+]);
