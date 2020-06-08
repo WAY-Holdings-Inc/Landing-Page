1b55c9e770a5e26c055acd4a0eea10a4a65ffa22
Sydney Lawrence <sll224@cornell.edu>
2020-06-07 21:17:51 -0400

    Add terms and privacy pages, navbar

---
+++ app/src/components/Privacy/Privacy.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Container, Row, Col } from 'react-bootstrap';
+import Footer from '../Footer/Footer';
+
+function Privacy() {
+  return (
+    <div>
+      <Container className="way-containery text-center">
+        <Row>
+          <Col lg={12}>
+            <h1>PRIVACY POLICY</h1>
+            <p>TODO fill in privacy policy</p>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  )
+}
+
+export default Privacy
---
+++ app/src/components/Terms/Terms.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Container, Row, Col } from 'react-bootstrap';
+import Footer from '../Footer/Footer';
+
+function Privacy() {
+  return (
+    <div>
+      <Container className="way-containery text-center">
+        <Row>
+          <Col lg={12}>
+
+            <article>
+              <h1>TERMS OF SERVICE</h1>
+              <p>TODO fill in terms of service</p>
+            </article>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  )
+}
+
+export default Privacy
