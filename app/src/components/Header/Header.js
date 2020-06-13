0845ef42a3c3170454fe733c3f1896803845af40
Jonna <jonna7chen@gmail.com>
2020-06-13 17:33:03 -0400

    fixed scrolling issue

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -4,10 +4,6 @@ import { HashLink as Link } from 'react-router-hash-link';
 import './Header.css';
 import logo from './assets/waylogo.svg';
 
-
-
-
-
 function Header(props) {
   const [expanded, setExpanded] = React.useState(false);
 
@@ -17,21 +13,21 @@ function Header(props) {
   }
   return (
     <Navbar expanded={expanded} className={`way-nav ${simpleNav.class}`} collapseOnSelect expand="xl" bg="white" variant="light">
-      <Navbar.Brand className="way-logo"><Link className="unset" to="/"><img src={logo} alt="Way Logo"/></Link></Navbar.Brand>
+      <Navbar.Brand className="way-logo"><Link className="unset" to="/"><img src={logo} alt="Way Logo" /></Link></Navbar.Brand>
       {
-        simple? '' :
-        <React.Fragment>
-          <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")}  aria-controls="responsive-nav" />
-          <Navbar.Collapse className="justify-content-end way-options" id="responsive-nav">
-            <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link className="unset" to="/#How">How it Works</Link></Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link className="unset" to="/#benefits">Benefits</Link></Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link className="unset" to="/about">About</Link></Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link className="unset" to="/contact">Contact Us</Link></Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="way-button"><Link className="unset" to="/start">Get Started</Link></Nav.Link>
-          </Navbar.Collapse>
-        </React.Fragment>
+        simple ? '' :
+          <React.Fragment>
+            <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="responsive-nav" />
+            <Navbar.Collapse className="justify-content-end way-options" id="responsive-nav">
+              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link className="unset" to="/#How">How it Works</Link></Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link className="unset" to="/#benefits">Benefits</Link></Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link className="unset" to="/about">About</Link></Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link className="unset" to="/contact">Contact Us</Link></Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="way-button"><Link className="unset" to="/start">Get Started</Link></Nav.Link>
+            </Navbar.Collapse>
+          </React.Fragment>
       }
-  </Navbar>
+    </Navbar>
   );
 }
 
