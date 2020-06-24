28c49ec4ec7e8667e98b2c8c90cca0f9e767aba8
Sydney Lawrence <sll224@cornell.edu>
2020-06-24 19:22:53 -0400

    Add build with custom 404

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9918f45dcf7266780bbed3b96c632868.js"
+  "/precache-manifest.1caecb89579123b234c6e47fdd99adcd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
