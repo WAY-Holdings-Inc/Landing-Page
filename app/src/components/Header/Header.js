80f6f8df361938bf3953eb32cec2a318129ba0cd
Jonna <jonna7chen@gmail.com>
2020-07-03 15:58:50 -0400

    fixed console error

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -20,11 +20,11 @@ function Header(props) {
           <React.Fragment>
             <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="responsive-nav" />
             <Navbar.Collapse className="justify-content-end way-options" id="responsive-nav">
-              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link smooth={true} to="/#How">How it Works</Link></Nav.Link>
-              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link smooth={true} to="/#benefits">Benefits</Link></Nav.Link>
-              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link to="/about">About</Link></Nav.Link>
-              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option"><Link to="/contact-us">Contact Us</Link></Nav.Link>
-              <Nav.Link onClick={() => setExpanded(false)} data-toggle="collapse" className="way-button"><Link to="/get-started">Get Started</Link></Nav.Link>
+              <Nav.Link as={Link} onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option" smooth={true} to="/#How">How it Works</Nav.Link>
+              <Nav.Link as={Link} onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option" smooth={true} to="/#benefits">Benefits</Nav.Link>
+              <Nav.Link as={Link} onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option" to="/about">About</Nav.Link>
+              <Nav.Link as={Link} onClick={() => setExpanded(false)} data-toggle="collapse" className="nav-option" to="/contact-us">Contact Us</Nav.Link>
+              <Nav.Link as={Link} onClick={() => setExpanded(false)} data-toggle="collapse" className="way-button" to="/get-started">Get Started</Nav.Link>
             </Navbar.Collapse>
           </React.Fragment>
       }
